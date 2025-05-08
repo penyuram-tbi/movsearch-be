2ac79036259cf91e9e66dd0b90c4d8c50154059a
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2025-05-09 00:39:03 +0700

    hide env file for api and url

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,25 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Movie Search API"
     
     # Elasticsearch configuration
-    ELASTICSEARCH_URL: str = "https://my-elasticsearch-project-dab849.es.us-central1.gcp.elastic.cloud:443"
-    ELASTICSEARCH_API_KEY: str = "R0RWZnI1WUJfcGdtMkxLNGd0dGU6OXBtR3kyNncyZkVNQjFjcXAwODd5UQ=="
+    ELASTICSEARCH_URL: str
+    ELASTICSEARCH_API_KEY: str
     INDEX_NAME: str = "semantic_documents"
     
     # Vector model configuration
     VECTOR_MODEL_NAME: str = "all-MiniLM-L6-v2"
     VECTOR_DIMENSIONS: int = 384
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+        # Prioritaskan environment variables dari sistem (termasuk GitHub Secrets)
+        env_prefix = ""
+        extra = "ignore"
 
 settings = Settings()
